95fa94741443d459f8fb60b8d7fa396eef93a127
Bozena <crazyhappygame@gmail.com>
2018-05-24 22:57:42 +0200

    Fix travis CI

--- tools/travis-scripts/run-script.sh
+++ tools/travis-scripts/run-script.sh
@@ -2,6 +2,7 @@
 
 # exit this script if any commmand fails
 set -e
+set -x
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 COCOS2DX_ROOT="$DIR"/../..
@@ -10,9 +11,9 @@ CPU_CORES=4
 function build_linux()
 {
     echo "Building tests ..."
+    echo $TRAVIS_BUILD_DIR
+    mkdir -p $COCOS2DX_ROOT/build
     cd $COCOS2DX_ROOT/build
-    mkdir -p build
-    cd build
     cmake ..
     cmake --build .
 }
