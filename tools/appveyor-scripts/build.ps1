570ac9dad1778687a0a8cc96b0c7dedb8251f069
Bozena <crazyhappygame@gmail.com>
2018-05-24 22:49:42 +0200

    Fix CI build scripts

--- tools/appveyor-scripts/build.ps1
+++ tools/appveyor-scripts/build.ps1
@@ -1,7 +1,7 @@
 Set-PSDebug -Trace 1
 
-mkdir $env:APPVEYOR_BUILD_FOLDER\build
-if ($lastexitcode -ne 0) {throw}
+& mkdir $env:APPVEYOR_BUILD_FOLDER\build
+# if ($lastexitcode -ne 0) {throw}
 Push-Location $env:APPVEYOR_BUILD_FOLDER\build
 & cmake ..
 if ($lastexitcode -ne 0) {throw}
