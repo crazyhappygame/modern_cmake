d45e51b800b08d2cebdda8ef583281a0b773f235
qqqq <email@example.com>
2018-06-01 21:28:45 +0200

    brew upgrade cmake

--- tools/travis-scripts/run-script.sh
+++ tools/travis-scripts/run-script.sh
@@ -83,7 +83,7 @@ function android_game12()
 function build_ios()
 {
     cmake --version
-    brew install cmake
+    brew upgrade cmake
     cmake --version
     mkdir -p $COCOS2DX_ROOT/build_ios
     cd $COCOS2DX_ROOT/build_ios
